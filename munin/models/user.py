a09d5e803e842aa7dd00b684250d98478b42148b
Mihir Singh <git.service@mihirsingh.com>
2016-03-10 16:07:55 -0500

    Implemented authentication of existing users by password

--- munin/models/user.py
+++ munin/models/user.py
@@ -12,3 +12,17 @@ class User(object):
         password = password.decode('utf-8')
 
         database.query(query, username=username, password=password)
+
+    @staticmethod
+    def authenticate(username, password):
+        query = 'SELECT password FROM users WHERE username=:username'
+
+        try:
+            hashed_password = database.query(query, username=username).all()[0].password
+
+            if bcrypt.hashpw(password.encode('utf-8'), hashed_password.encode('utf-8')) == hashed_password.encode('utf-8'):
+                return True
+        except Exception as e:
+            raise e
+
+        return False
