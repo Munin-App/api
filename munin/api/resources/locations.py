937810cbf6852e47a7f252076d1daa4a32882657
Mihir Singh <git.service@mihirsingh.com>
2016-03-11 15:52:02 -0500

    Implemented support for GET against /locations/

--- munin/api/resources/locations.py
+++ munin/api/resources/locations.py
@@ -1,6 +1,7 @@
 import falcon
 from munin.models import Token, Location
 import json
+import datetime
 
 
 class LocationResource(object):
@@ -31,3 +32,41 @@ class LocationResource(object):
         except Exception as e:
             resp.status = falcon.HTTP_500
             resp.body = json.dumps({'error': str(e)})
+
+    def on_get(self, req, resp):
+        limit = req.params.get('limit', 100)
+        offset = req.params.get('offset', 0)
+        token = req.get_header('X-Authorization', req.params.get('token', None))
+
+        if token is None:
+            resp.status = falcon.HTTP_401
+            return
+
+        token = Token.authenticate(token)
+
+        if token is None:
+            resp.status = falcon.HTTP_401
+            return
+
+        try:
+            locations = Location.select().where(Location.user_id == token.user_id)
+
+            response = {
+                'meta': {
+                    'offset': offset,
+                    'limit': limit,
+                    'total': Location.select().count()
+                },
+                'locations': []
+            }
+
+            for location in locations.offset(offset).limit(limit):
+                response['locations'].append({
+                    'latitude': location.latitude,
+                    'longitude': location.longitude,
+                    'timestamp': datetime.datetime.strftime(location.timestamp, '%Y-%m-%dT%H:%M:%S.%fZ')
+                })
+
+            resp.body = json.dumps(response)
+        except:
+            resp.status = falcon.HTTP_500
